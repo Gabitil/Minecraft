e823c925b24fb058717a7bbd36b219cdf0a8f38f
Gabitil <gabriel.augusto3m2@gmail.com>
2023-12-19 09:43:25 -0300

    Update cond_farm_ferro.lua

--- cond_farm_ferro.lua
+++ cond_farm_ferro.lua
@@ -1,8 +1,8 @@
 -- Programa para verificar continuamente se o sinal de redstone e o baú estão vazios
 
 -- Defina o lado em que o sinal de redstone está chegando e o lado em que o baú está conectado
-local ladoRedstone = "direita"  -- Substitua com o lado correto
-local ladoBau = "esquerda"      -- Substitua com o lado correto
+local ladoRedstone = "left"  -- Substitua com o lado correto
+local ladoBau = "right"      -- Substitua com o lado correto
 
 -- Função para verificar se o sinal do pulso de redstone está ativo (true) ou não (false)
 local function redstoneAtivo()
@@ -19,11 +19,11 @@ while true do
   -- Lógica AND: A ação só ocorre se ambos os sinais estiverem ativos
   if redstoneAtivo() and bauVazio() then
     -- Coloque aqui a lógica para ligar a máquina
-    redstone.setOutput("frente", true)
+    redstone.setOutput("back", true)
     print("Ligando a máquina!")
   else
     print("Condição não atendida. Não é possível ligar a máquina.")
-    redstone.setOutput("frente", false)
+    redstone.setOutput("back", false)
   end
   
   -- Aguarde um curto período antes de verificar novamente (evitar loop muito rápido)
