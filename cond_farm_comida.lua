af68267a1b6b8003fee51e9b808a2bad7df4eab5
Gabitil <gabriel.augusto3m2@gmail.com>
2023-12-21 17:21:25 -0300

    Update cond_farm_comida.lua
    Raw não atualizou então refiz o código pra ficar em portguês as variaveis

--- cond_farm_comida.lua
+++ cond_farm_comida.lua
@@ -1,17 +1,20 @@
 -- Desc: Controla o funcionamento do plantador de comida
 
+local esquerda = "left" -- Saída do sinal de redstone
+local direita = "right" -- Entrada do sinal de redstone
+local atras = "back" -- Entrada do sinal de redstone
 while true do
   -- Verifica o sinal de redstone atrás
-  local sinalAtras = redstone.getInput("back")
+  local sinalAtras = redstone.getInput(atras)
 
   -- Verifica o sinal de redstone à direita
-  local sinalDireita = redstone.getInput("right")
+  local sinalDireita = redstone.getInput(direita)
 
   -- Atualiza o estado do flip-flop com base nos sinais
   if sinalAtras then
-    redstone.setOutput("left", false)  -- Desliga o sinal à esquerda
+    redstone.setOutput(esquerda, false)  -- Desliga o sinal à esquerda
   elseif sinalDireita then
-    redstone.setOutput("left", true)  -- Liga o sinal à esquerda
+    redstone.setOutput(esquerda, true)  -- Liga o sinal à esquerda
   end
 
   -- Aguarda um curto período antes de verificar novamente
