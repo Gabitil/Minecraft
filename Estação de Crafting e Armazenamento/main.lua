c401f4e43431b99ef4925e8c3d67ab379a4c02bd
Gabriel <61718293+Gabitil@users.noreply.github.com>
2024-11-01 11:17:10 -0300

    teste2

--- Estação de Crafting e Armazenamento/main.lua
+++ Estação de Crafting e Armazenamento/main.lua
@@ -2,9 +2,10 @@
 local chests = {}
 
 -- Procura por perifericos do tipo "minecraft:chest" e adiciona cada um à tabela 'chests'
-for _, name in ipairs(peripheral.getNames()) do
+for _, name in ipairs(peripheral.getNames()) do -- 'ipairs' retorna uma tabela com os indices e valores da tabela 'peripheral.getNames()'
     if peripheral.hasType(name, "minecraft:chest") then
         table.insert(chests, peripheral.wrap(name))
+        print("resultado wrap" .. peripheral.wrap(name))
     end
 end
 
@@ -12,7 +13,9 @@ end
 for i, chest in ipairs(chests) do
     print("Bau " .. i .. ":")
     local items = chest.list()
-    for slot, item in pairs(items) do
+    for slot, item in pairs(items) do -- 'pairs' retorna uma tabela com os indices e valores da tabela 'items'
         print("Slot " .. slot .. ": " .. item.name .. " x" .. item.count)
     end
+
+
 end
