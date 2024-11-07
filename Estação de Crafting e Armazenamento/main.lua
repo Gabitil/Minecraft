a31dfc43cf6af25e11081a451024ef9438bc57e6
Gabriel <61718293+Gabitil@users.noreply.github.com>
2024-11-07 13:40:51 -0300

    teste impressora v2

--- Estação de Crafting e Armazenamento/main.lua
+++ Estação de Crafting e Armazenamento/main.lua
@@ -1,26 +1,34 @@
 -- Tabela para armazenar os baus
-local printer = peripheral.wrap("right")
+local printer = peripheral.find("printer")
 local chests = {}
-
+i=1
+printer.newPage()
+printer.setPageTitle("Teste impressora")
 -- Procura por perifericos do tipo "minecraft:chest" e adiciona cada um à tabela 'chests'
 for _, name in ipairs(peripheral.getNames()) do -- 'ipairs' retorna uma tabela com os indices e valores da tabela 'peripheral.getNames()'
     if peripheral.hasType(name, "minecraft:chest") then
         table.insert(chests, peripheral.wrap(name))
-        printer.newpage()
+       
         printer.write("resultado wrap", peripheral.wrap(name))
-        printer.endpage()
+        printer.setCursorPos(1,i)
+        i+1
+
     end
 end
 
 -- Exemplo: acessando itens de todos os baús
 for i, chest in ipairs(chests) do
-    printer.newpage()
+
     printer.write("Bau " .. i .. ":")
+    printer.setCursorPos(1,i)
+    i+1
     local items = chest.list()
     for slot, item in pairs(items) do -- 'pairs' retorna uma tabela com os indices e valores da tabela 'items' asfdasfasfasdas
         printer.write("Slot " .. slot .. ": " .. item.name .. " x" .. item.count)
+        printer.setCursorPos(1,i)
+        i+1
     end
-    printer.endpage()
+
 
 end
-
+printer.endPage()
