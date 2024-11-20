8f68d9cd723915a864e6b563845b9ed5bd87aa57
Gabriel <61718293+Gabitil@users.noreply.github.com>
2024-11-20 12:57:54 -0300

    adiciona validação para garantir que a transferência de itens não exceda os limites do baú

--- Estação de Crafting e Armazenamento/main.lua
+++ Estação de Crafting e Armazenamento/main.lua
@@ -7,6 +7,18 @@ local itensNaoCompletos = {} -- Lista de itens não completos
 -- Abre o arquivo de log para salvar a saída
 local logFile = fs.open("saida.txt", "w")
 
+-- Redefines the `print` function to log messages to a file.
+-- 
+-- This function takes any number of arguments, converts them to strings,
+-- and writes them to a log file, separated by tabs. Each call to `print`
+-- ends with a newline in the log file.
+-- 
+-- Parameters:
+-- ... (vararg): A variable number of arguments to be printed and logged.
+-- 
+-- Note:
+-- The `logFile` variable must be a valid file handle that supports `write`
+-- and `flush` methods.
 -- Redefine a função `print` para registrar as mensagens no arquivo
 function print(...)
     local args = {...}
@@ -20,7 +32,24 @@ end
 -- Captura erros usando pcall para salvar no arquivo também
 local status, err = pcall(function()
 
-    --função  que recebe a lista chests para procurar itens não completos
+    --[[
+        Function: procurarItensNaoCompletos
+        Description: Procura por itens incompletos em uma lista de baús e adiciona-os a uma lista de itens incompletos.
+        
+        Parameters:
+            chests (table): Uma tabela contendo os baús a serem verificados.
+        
+        Returns:
+            None
+        
+        Details:
+            - Itera sobre cada baú na lista fornecida.
+            - Para cada baú, itera sobre cada slot e item no baú.
+            - Obtém os detalhes completos do item no slot atual.
+            - Verifica se o item é incompleto (quantidade menor que o limite do slot).
+            - Adiciona os itens incompletos a uma lista chamada `itensNaoCompletos`.
+            - Imprime no console os detalhes dos itens incompletos encontrados.
+    ]]
     local function procurarItensNaoCompletos(chests)
         for _, chest in ipairs(chests) do
             for slot, item in pairs(chest.list()) do
@@ -34,89 +63,110 @@ local status, err = pcall(function()
         end
     end
 
-    --função para conferir se tem mais de um item igual incompleto, se não tiver exclui da lista de procurar
+    --[[
+        Função: conferirItensNaoCompletos
+        Descrição: Verifica se há itens duplicados na lista de itens incompletos e mantém apenas os itens duplicados na lista.
+        Parâmetros: Nenhum
+        Retorno: Nenhum
+        Detalhes:
+            - Itera sobre a lista de itens incompletos (itensNaoCompletos).
+            - Para cada item, verifica se existe outro item com o mesmo nome (displayName) mas em um slot diferente.
+            - Se encontrar um item duplicado, adiciona-o à lista de itens para manter (itensParaManter).
+            - Atualiza a lista de itens incompletos (itensNaoCompletos) com apenas os itens duplicados.
+            - Imprime no console uma mensagem indicando o item duplicado encontrado, incluindo seu nome, quantidade, slot e baú.
+    ]]
     local function conferirItensNaoCompletos()
-        local itensParaManter = {} -- Lista de itens para manter
-    
+        local temIgual = false
         -- Itera sobre a lista de itens incompletos
+        ::repete::
         for i, item in ipairs(itensNaoCompletos) do
-            local temIgual = false
-    
             -- Verifica se existe um item igual na lista
             for j, item2 in ipairs(itensNaoCompletos) do
                 if i ~= j and item.itemDetail.displayName == item2.itemDetail.displayName then -- Verifica se o nome é igual e o slot é diferente
                     temIgual = true
-                    break -- Encontrou um item igual, então não precisa continuar verificando
                 end
             end
-    
-            -- Se tiver item igual, adiciona à lista de itens para manter
-            if temIgual then
-                table.insert(itensParaManter, item) -- Adicionando à lista de itens para manter
-                print("Item duplicado encontrado: " .. item.itemDetail.displayName .. " " .. item.itemDetail.count .. " slot " .. item.slot .. " bau " .. peripheral.getName(item.chest))
+            
+            if temIgual == false then
+                table.remove(itensNaoCompletos, i) -- Remove o item da lista de itens incompletos
+                goto repete
             end
         end
-    
-        -- Atualiza a lista de itens não completos com apenas os duplicados
-        itensNaoCompletos = itensParaManter
+
     end
 
-    -- Verifica se um item existe na tabela
-    local function itemExists(tabela, itemToFind)
-        for _, item in ipairs(tabela) do
-            if item.chest == itemToFind.chest and 
-               item.slot == itemToFind.slot and
-               item.item == itemToFind.item and
-               item.itemDetail.displayName == itemToFind.itemDetail.displayName and
-               item.itemDetail.count == itemToFind.itemDetail.count then
-                return true
-            end
-        end
-        return false
-    end
     
+    --[[
+        Função: transferirItens
+        Descrição: Transfere itens entre dois baús, completando stacks incompletos.
+        Parâmetros: Nenhum.
+        Retorno: Nenhum.
+        Detalhes:
+            - Verifica se há itens incompletos na lista `itensNaoCompletos`.
+            - Faz uma cópia da tabela `itensNaoCompletos` para `itensNaoFinalizados`.
+            - Itera sobre a lista de itens incompletos e verifica se o item já foi finalizado.
+            - Se o item não foi finalizado, itera sobre a lista de itens não finalizados.
+            - Verifica se o nome do item é igual e o slot é diferente.
+            - Calcula a quantidade que falta para completar o stack.
+            - Transfere a quantidade necessária de itens entre os baús.
+            - Adiciona o item à lista de itens finalizados e remove da lista de itens não finalizados.
+            - Imprime mensagens de log detalhando a transferência.
+    ]]
     --função que recebe dois baús e transfere os itens
     local function transferirItens()
-
+        ::reiniciar::
         -- Verifica se não há itens incompletos
-        if #itensNaoCompletos == 0 then
+        print("Tamanho da lista de itens incompletos: " .. #itensNaoCompletos)
+        if #itensNaoCompletos <= 1 then
             print("Nao ha itens incompletos para transferir.")
             return
         end
-        -- Fazer uma cópia da tabela itensNaoCompletos
-        local itensNaoFinalizados = {} -- Lista de itens não finalizados
-        for i, item in ipairs(itensNaoCompletos) do
-            table.insert(itensNaoFinalizados, item) -- Adiciona à lista de itens não finalizados
-        end
-
-        local itemfinalizado = {} -- Lista de itens finalizados
-
         -- Itera sobre a lista de itens incompletos
+        
         for i, item in ipairs(itensNaoCompletos) do
 
-            if itemExists(itemfinalizado, item) then -- Verifica se o item já foi finalizado
-                break
-            end
-                for j, item2 in ipairs(itensNaoFinalizados) do -- Itera sobre a lista de itens não finalizados
-                    if item.slot ~= item2.slot and item.itemDetail.displayName == item2.itemDetail.displayName and item.itemDetail.count < item.chest.getItemLimit(item.slot) and item.itemDetail.count > 0 then -- Verifica se o nome é igual e o slot é diferente
+                -- Itera sobre a lista de itens incompletos
+                for j, item2 in ipairs(itensNaoCompletos) do
+                    local msmbs = false
+
+                    if item.chest == item2.chest and item.slot == item2.slot then
+                        msmbs = true -- Verifica se é o mesmo bau e slot
+                    end
+
+                    if msmbs == false and item.itemDetail.displayName == item2.itemDetail.displayName and item.itemDetail.count < item.chest.getItemLimit(item.slot) and item.itemDetail.count > 0 then -- Verifica se o nome é igual e o slot é diferente
                         local qntfalta = item.chest.getItemLimit(item.slot) - item.itemDetail.count -- Calcula a quantidade que falta para completar o stack
                         local qntTrans = 0 -- Quantidade a ser transferida
 
                         if item2.itemDetail.count > qntfalta then -- Verifica se a quantidade do item2 é maior que a quantidade que falta
 
                             item.chest.pullItems(peripheral.getName(item2.chest), item2.slot, qntfalta, item.slot) -- Transfere a quantidade que falta
+                            item.itemDetail = item.chest.getItemDetail(item.slot) -- Atualiza os detalhes do item
+                            item2.itemDetail = item2.chest.getItemDetail(item2.slot) -- Atualiza os detalhes do item2
                             qntTrans = qntfalta
-                            table.insert(itemfinalizado, item) -- Adiciona o item à lista de itens finalizados
-                            table.remove(itensNaoFinalizados, i) -- Remove o item da lista de itens não finalizados
+                            table.remove(itensNaoCompletos, i) -- Remove o item da lista de itens incompletos
                             print("Transferindo " .. qntTrans .. " " .. item.itemDetail.displayName .. " do bau " .. peripheral.getName(item2.chest) .. " slot ".. item2.slot .." para o bau " .. peripheral.getName(item.chest) .. " slot ".. item.slot)
-                            break
+                            goto reiniciar
 
-                        else                            
-                            item.chest.pullItems(peripheral.getName(item2.chest), item2.slot, qntfalta, item.slot)
+                        elseif item2.itemDetail.count < qntfalta then 
+                            qntTrans = item2.itemDetail.count  
                             local falta = qntfalta - item2.itemDetail.count
-                            qntTrans = item2.itemDetail.count
+                            item.chest.pullItems(peripheral.getName(item2.chest), item2.slot, qntfalta, item.slot)
+                            item.itemDetail = item.chest.getItemDetail(item.slot) -- Atualiza os detalhes do item
+                            item2.itemDetail = item2.chest.getItemDetail(item2.slot) -- Atualiza os detalhes do item2
+                            table.remove(itensNaoCompletos, j) -- Remove o item da lista de itens incompletos 
                             print("Transferindo " .. qntTrans .. " " .. item.itemDetail.displayName .. " do bau " .. peripheral.getName(item2.chest) .. " slot ".. item2.slot .. " para o bau " .. peripheral.getName(item.chest) .. " slot ".. item.slot .. " falta " .. falta)
-                        end
+                            goto reiniciar
+
+                        else
+                            item.chest.pullItems(peripheral.getName(item2.chest), item2.slot, qntfalta, item.slot) -- Transfere a quantidade que falta
+                            item.itemDetail = item.chest.getItemDetail(item.slot) -- Atualiza os detalhes do item
+                            item2.itemDetail = item2.chest.getItemDetail(item2.slot) -- Atualiza os detalhes do item2
+                            qntTrans = qntfalta
+                            table.remove(itensNaoCompletos, i) -- Remove o item da lista de itens incompletos
+                            table.remove(itensNaoCompletos, j) -- Remove o item da lista de itens incompletos
+                            print("Transferindo " .. qntTrans .. " " .. item.itemDetail.displayName .. " do bau " .. peripheral.getName(item2.chest) .. " slot ".. item2.slot .." para o bau " .. peripheral.getName(item.chest) .. " slot ".. item.slot)
+                            goto reiniciar
+                        end    
                     end
                 end      
         end
@@ -125,6 +175,42 @@ local status, err = pcall(function()
 
 
 
+    --[[
+        This script performs several operations related to peripherals in a Minecraft environment.
+        
+        The main operations include:
+        1. Iterating over connected peripherals to find chests.
+        2. Searching for incomplete items in the chests.
+        3. Checking for duplicate incomplete items.
+        4. Transferring items to complete them.
+        5. Logging the time taken for each operation and the total execution time.
+        
+        Functions:
+        - peripheral.getNames(): Retrieves the names of all connected peripherals.
+        - peripheral.hasType(name, "minecraft:chest"): Checks if the peripheral is a chest.
+        - peripheral.wrap(name): Wraps the peripheral for further operations.
+        - procurarItensNaoCompletos(chests): Searches for incomplete items in the provided chests.
+        - conferirItensNaoCompletos(): Checks for duplicate incomplete items.
+        - transferirItens(): Transfers items to complete them.
+        
+        Variables:
+        - inicioPerifericos: Start time for peripheral search.
+        - tempoPerifericos: Time taken for peripheral search.
+        - inicioContagem: Start time for item search.
+        - tempoContagem: Time taken for item search.
+        - inicioConferir: Start time for checking items.
+        - tempoConferir: Time taken for checking items.
+        - inicioTransferencia: Start time for item transfer.
+        - tempoTransferencia: Time taken for item transfer.
+        - endTime: End time for the entire execution.
+        - tempoExecucao: Total execution time.
+        
+        Error Handling:
+        - Logs any errors encountered during execution to a log file.
+        
+        Note:
+        - The total execution time is divided by 20 for some reason, possibly related to the game's tick rate.
+    ]]
     -- Itera sobre os perifericos conectados
     local inicioPerifericos = os.time()
     for _, name in ipairs(peripheral.getNames()) do
